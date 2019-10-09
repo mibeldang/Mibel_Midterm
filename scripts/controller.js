cdcc927ac4ab5ea284120ab8a5b6f508f975261c
GeniusThea <bejectanya123@gmail.com>
2019-10-09 11:17:43 +0800

    midterms

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,11 +3,12 @@ client.on("connect", function () {
   client.subscribe("mibel/fan/status");
   $("button").click(function () {
     var id = $(this).attr("id");
-    client.publish("mibel/fan/status", "The fan is currently turned " + id);
+    client.publish("mibel/fan/status", "The fan is currently turned " + id+"_"+(moment().format('MMMM Do YYYY, h:mm:ss a')));
   })
-
   client.on("message", function (topic, payload) {
-    $("#status").text(payload)
+    var payload = payload.toString().split("_")
+    console.log(payload.join(","))
+    $("#status").text(payload[0])
   })
 
 })
